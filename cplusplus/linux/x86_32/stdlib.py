e6641818aa8d6a3f936e930f21a3456b3861ff61
Asmeble <dylan.k.eliot@gmail.com>
2020-01-27 15:51:16 -0600

    Update stdlib.py

--- cplusplus/linux/x86_32/stdlib.py
+++ cplusplus/linux/x86_32/stdlib.py
@@ -1,3 +1,21 @@
 #modelled after [stdlib.h]
+
+from __builtins__ import abs
+
+def atexit(void_type):
+  return void_type()
+
+def atof(str_):
+  return float(str_)
+
+def atoi(str_):
+  return int(str_)
+
+def atol(str_):
+  return atoi(str_)
+
+def atoll(str_):
+  return atol(str_)
+
 def malloc(size):
   return bytearray(bytes(size))
