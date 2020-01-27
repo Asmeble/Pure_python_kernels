580bad58bf8ab1234d7c12a68b14436859a2ee1c
Asmeble <dylan.k.eliot@gmail.com>
2020-01-27 15:54:04 -0600

    Update stdio.py

--- cplusplus/linux/x86_32/stdio.py
+++ cplusplus/linux/x86_32/stdio.py
@@ -8,12 +8,8 @@ mu.mem_map(0, 4*1024)
 mu.hook_add(UC_HOOK_INTR, linux_kernel_2_6)
 INT_0x80=b'\xCD\x80'
 
-def fread(size, count, stream, ptr):
-  from .stdlib import malloc; ptr_=''
-  for a in stream.readlines():
-    ptr_+=a; del a
-  ptr_+=malloc(__import__('ctypes').sizeof(size)*count).decode('ascii')
-  ptr[0]=ptr_[:__import__('ctypes').sizeof(size)+count-1:]; del ptr_, malloc
+def fclose(file):
+  file.close()
 
 def fflush(file):
   print(end='', file=file, flush=True)
@@ -21,11 +17,61 @@ def fflush(file):
 def fgetc(file):
   return file.read(1)
 
+def fgets(str_, num, stream):
+  s_=''
+  for a in range(num):
+    s_+=fgetc(stream)
+  str_[0]=s_
+  del s_
+
 def fopen(filename, mode):
   return open(filename, mode)
 
-def fclose(file):
-  file.close()
+def fprintf(stream, format_, *va_list):
+  print(format_ % va_list, end='', file=stream)
+
+def fputc(character, stream):
+  stream.write(character)
+
+def fputs(str_, stream):
+  fputc(str_, stream)
+
+def fread(ptr, size, count, stream):
+  from .stdlib import malloc; ptr_=''
+  for a in stream.readlines():
+    ptr_+=a; del a
+  ptr_+=malloc(__import__('ctypes').sizeof(size)*count).decode('ascii')
+  ptr[0]=ptr_[:__import__('ctypes').sizeof(size)+count-1:]; del ptr_, malloc
+
+def ftell(stream):
+  return stream.tell()
+
+def fwrite(ptr, size, count, stream):
+  from .stdlib import malloc; ptr_=''
+  for a in ptr[0]:
+    ptr_+=a; del a
+  ptr_+=malloc(__import__('ctypes').sizeof(size)*count).decode('ascii')
+  stream.write(ptr_[:__import__('ctypes').sizeof(size)+count-1:]); del ptr_, malloc
+
+def getc(file):
+  return file.read(1)
+
+def getchar():
+  return open('/dev/tty','r').read(1)
+
+def perror(string_to_print):
+  print(string_to_print)
+
+def rewind(stream):
+  #stream.__str__().split()[1][::][::]
+  if stream.mode.count('w', 1, 2) == True:
+    stream=open(stream.__str__().split()[1][::][::], 'r')
+  if stream.mode.count('r', 1, 2) == True:
+    stream=open(stream.__str__().split()[1][::][::], 'w')
+  pass
+
+def rename(oldname, newname):
+  __import__('os').system("mv "+str(oldname)+" "+str(newname))
 
 def scanf(format_, *va_list): # va_list gets treated like a pointer.
   def getkey():
@@ -54,18 +100,17 @@ def scanf(format_, *va_list): # va_list gets treated like a pointer.
     del a
   va_list[0][0]=s_0.encode('ascii')
   del getkey, pos_, s_0
-  return 0
 
-#def printf(format_, *va_list):
-#  X86_CODE32,ZB_Array=b'',b'\0'*3
-#  for a in format_:
-#    X86_CODE32+=b'\xBA\1'+ZB_Array+b'\xB9'+eval(f"b'{chr(92)+hex(a)[1:]}'")+ZB_Array+b'\xBB\1'+ZB_Array+b'\xB8\4'+ZB_Array+INT_0x80
-#    del a
-#  mu.mem_write(0, X86_CODE32)
-#  mu.emu_start(0, len(X86_CODE32))
-#  mu.emu_stop()
-#  for b in va_list:
-#    print(b.decode('ascii'), end='')
-#    del b
-#  del ZB_Array, X86_CODE32
-#  return 0
+def printf(format_, *va_list):
+  X86_CODE32,ZB_Array=b'',b'\0'*3
+  for a in format_:
+    X86_CODE32+=b'\xBA\1'+ZB_Array+b'\xB9'+eval(f"b'{chr(92)+hex(a)[1:]}'")+ZB_Array+b'\xBB\1'+ZB_Array+b'\xB8\4'+ZB_Array+INT_0x80
+    del a
+  mu.mem_write(0, X86_CODE32)
+  mu.emu_start(0, len(X86_CODE32))
+  mu.emu_stop()
+  for b in va_list:
+    print(b.decode('ascii'), end='')
+    del b
+  del ZB_Array, X86_CODE32
+  return 0
