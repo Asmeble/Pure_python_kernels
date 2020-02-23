1cb814ac8e8fe92724ae6c8b249a74bf2510f39f
Asmeble <dylan.k.eliot@gmail.com>
2020-02-23 12:23:55 -0600

    Update stdio.py

--- cplusplus/linux/x86_32/stdio.py
+++ cplusplus/linux/x86_32/stdio.py
@@ -4,8 +4,6 @@ from requests import get as wget
 def Github_import(username, repo, branch, path_to_module):
   return wget("https://raw.githubusercontent.com/"+username+"/"+repo+"/"+branch+"/"+path_to_module).text
 
-global unicorn
-unicorn=None
 exec(Github_import(username="Asmeble",repo="unicorn",branch="master", path_to_module="bindings/python/unicorn/unicorn.py"))
 #exec(repr(Github_import(username="Asmeble",repo="unicorn",branch="master", path_to_module="bindings/python/unicorn/__init__.py")))
 from unicorn import Uc, UC_ARCH_X86, UC_MODE_32, UC_HOOK_INTR
