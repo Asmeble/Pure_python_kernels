56c54d47425b0219f9bac56c9f5b3b933d7dd4cb
Asmeble <dylan.k.eliot@gmail.com>
2020-02-23 12:17:20 -0600

    Update stdio.py

--- cplusplus/linux/x86_32/stdio.py
+++ cplusplus/linux/x86_32/stdio.py
@@ -4,13 +4,13 @@ from requests import get as wget
 def Github_import(username, repo, branch, path_to_module):
   return wget("https://raw.githubusercontent.com/"+username+"/"+repo+"/"+branch+"/"+path_to_module).text
 
-eval(Github_import(username="Asmeble",repo="unicorn",branch="master", path_to_module="bindings/python/unicorn/unicorn.py"))
+global unicorn
+
+exec(repr(Github_import(username="Asmeble",repo="unicorn",branch="master", path_to_module="bindings/python/unicorn/unicorn.py")))
 #exec(repr(Github_import(username="Asmeble",repo="unicorn",branch="master", path_to_module="bindings/python/unicorn/__init__.py")))
+from unicorn import Uc, UC_ARCH_X86, UC_MODE_32, UC_HOOK_INTR
 
 exec(repr(Github_import(username="Asmeble",repo="The_wrecking_ball",branch="v1-beta_2.22.2020", path_to_module="linux_kernels/x86_32/kernel_functions.py")))
-
-
-from unicorn import Uc, UC_ARCH_X86, UC_MODE_32, UC_HOOK_INTR
 
 mu=Uc(UC_ARCH_X86, UC_MODE_32)
 mu.mem_map(0, 4*1024)
