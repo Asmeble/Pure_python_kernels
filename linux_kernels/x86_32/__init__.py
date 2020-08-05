323a90227bb68f2b937ee52cb6c1fc2be270545d
Asmeble <dylan.k.eliot@gmail.com>
2020-08-04 19:18:07 -0500

    Update __init__.py

--- linux_kernels/x86_32/__init__.py
+++ linux_kernels/x86_32/__init__.py
@@ -6,4 +6,5 @@ Welcome to the virtual linux kernel 2.6+ in python.
 """
 #\x1b[4B\x1b[2A
 #\x1b[L
-from .L2_6_Kfunctions import *
+with __import__("github_import").Git_import(username="Asmeble", repo="The_wrecking_ball", branch="v-07.28.2020", path_to_module=x86_32_path+"L2_6_Kfunctions.py") as L2_6_Kfunctions:
+  sys_write=L2_6_Kfunctions.sys_write
