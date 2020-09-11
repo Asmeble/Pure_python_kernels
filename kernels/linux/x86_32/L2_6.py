ea40db7e999be8ae7a3cf0b254a6988020d126d1
Asmeble <dylan.k.eliot@gmail.com>
2020-09-11 17:47:19 -0500

    Update L2_6.py

--- kernels/linux/x86_32/L2_6.py
+++ kernels/linux/x86_32/L2_6.py
@@ -1,22 +1,24 @@
-from unicorn.x86_const import UC_X86_REG_EAX, UC_X86_REG_ECX, UC_X86_REG_EDX, UC_X86_REG_ESI, UC_X86_REG_EDI, UC_X86_REG_EBX
-from .__init__ import *
-sys_write=sys_write; # Perform sanity checks of functions ahead of time..... 
-UC_X86_REG_EAX=UC_X86_REG_EAX; UC_X86_REG_ECX=UC_X86_REG_ECX; UC_X86_REG_EDX=UC_X86_REG_EDX; UC_X86_REG_ESI=UC_X86_REG_ESI; UC_X86_REG_EDI=UC_X86_REG_EDI; UC_X86_REG_EBX=UC_X86_REG_EBX
+global linux_kernel_2_6
 def linux_kernel_2_6(uc, intno, user_data):
-  global UC_X86_REG_EAX, UC_X86_REG_ECX, UC_X86_REG_EDX, UC_X86_REG_ESI, UC_X86_REG_EDI, UC_X86_REG_EBX
+  global args_, args, Linux_kernel_functions_on_eax
   if intno == 0x80:
-    args,args_,function_lookup={u'ebx': uc.reg_read(UC_X86_REG_EBX), u'ecx': uc.reg_read(UC_X86_REG_ECX), u'edx': uc.reg_read(UC_X86_REG_EDX), u'esi': uc.reg_read(UC_X86_REG_ESI), u'edi': uc.reg_read(UC_X86_REG_EDI)},{},''
-    for arg in args:
-      if args[arg] != 0:
-        args_[arg]=args[arg]
-    args=args_
-    from kernels.linux.x86_32 import Linux_kernel_functions_on_eax
     try:
-      Linux_kernel_functions_on_eax.func_list.get(uc.reg_read(UC_X86_REG_EAX))(**args)
-    except NameError:
-      pass
+      from unicorn.x86_const import UC_X86_REG_EBX, UC_X86_REG_ECX, UC_X86_REG_EDX, UC_X86_REG_ESI, UC_X86_REG_EDI
+      args={
+        u'ebx': uc.reg_read(UC_X86_REG_EBX), 
+        u'ecx': uc.reg_read(UC_X86_REG_ECX), 
+        u'edx': uc.reg_read(UC_X86_REG_EDX),
+        u'esi': uc.reg_read(UC_X86_REG_ESI),
+        u'edi': uc.reg_read(UC_X86_REG_EDI)
+      }
+      list(map(lambda a, args=args: exec("del args[a]"), list(filter(lambda arg: args[arg] == 0, args))))
+      from . import Linux_kernel_functions_on_eax
+      Linux_kernel_functions_on_eax.func_list.get(uc.reg_read(__import__("unicorn").x86_const.UC_X86_REG_EAX))(**args)
     except OverflowError:
       print('', end='\n')
     finally:
-      del arg, args, args_, Linux_kernel_functions_on_eax
+      del intno, args, Linux_kernel_functions_on_eax, UC_X86_REG_EBX, UC_X86_REG_ECX, UC_X86_REG_EDX, UC_X86_REG_ESI, UC_X86_REG_EDI
       pass
+    return
+  return
+__all__=["linux_kernel_2_6"]
